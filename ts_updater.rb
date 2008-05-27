ed8ea848558980f1cdd19f5376c5897eed2ca23d
Michail Pishchagin <mblsha@siruba.(none)>
2008-05-28 01:38:52 +0400

    Separated .ts processor classes. Updated Message class to understand 'numerusform' translations and a couple of other <message> attributes.

--- ts_updater.rb
+++ ts_updater.rb
@@ -1,78 +1,13 @@
 #!/usr/bin/env ruby -w
 # Qt Linguist patcher by Michail Pishchagin
 
+require 'qt_linguist'
+
 require 'rubygems'
 require 'optparse'
 require 'ostruct'
-require 'extensions/all'
 require 'rio'
-require 'rexml/document'
-
-class Message
-  attr_accessor :source, :translation, :comment, :location_file, :location_line
-  attr_accessor :type
-  
-  def initialize(root)
-    @source = root.elements["source"].get_text
-    @translation = root.elements["translation"].get_text
-    @comment = root.elements["comment"]
-    @comment = @comment.get_text if @comment
-    location = root.elements["location"]
-    @location_file = location.attributes["filename"] if location
-    @location_line = location.attributes["line"] if location
-    @type = root.elements["translation"].attributes["type"]
-  end
-  
-  def to_s
-    "    <message>\n" +
-    (@location_file.nil? ? '' : "        <location filename=\"#@location_file\" line=\"#@location_line\"/>\n") +
-    "        <source>#@source</source>\n" +
-    (@comment.nil? ? '' : "        <comment>#@comment</comment>\n") +
-    "        <translation#{@type ? " type=\"#@type\"" : ''}>#@translation</translation>\n" +
-    "    </message>"
-  end
-end
-
-class Context
-  attr_accessor :name, :messages
-
-  def initialize(root)
-    @name = root.elements["name"].text
-    @messages = Array.new
-
-    root.elements.each("message") do |message|
-      @messages << Message.new(message)
-    end
-  end
-  
-  def to_s
-    "<context>\n" +
-    "    <name>#@name</name>\n" +
-    @messages.map { |e| e.to_s }.join("\n") + "\n" +
-    "</context>"
-  end
-end
-
-class TS
-  attr_accessor :contexts
-
-  def initialize(file_name)
-    doc = REXML::Document.new(File.new(file_name))
-    root = doc.elements[1]
-    @contexts = Array.new
-
-    root.elements.each("context") do |context|
-      @contexts << Context.new(context)
-    end
-  end
-
-  def to_s
-    "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-    "<!DOCTYPE TS><TS version=\"1.1\" language=\"ru_RU\">\n" +
-    @contexts.map { |e| e.to_s }.join("\n") + "\n" +
-    "</TS>\n"
-  end
-end
+# require 'extensions/all'
 
 class Worker
 private
